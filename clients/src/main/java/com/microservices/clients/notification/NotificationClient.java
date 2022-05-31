fc41160ebc53cefb566c28e3d18af19df3d0087f
Pratik Goswami <goswp004.304@gmail.com>
2022-05-31 19:16:18 +0100

    Refactoring microservice to kubernetes

--- clients/src/main/java/com/microservices/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/microservices/clients/notification/NotificationClient.java
@@ -3,7 +3,10 @@ package com.microservices.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
     void sendNotification(NotificationRequest notificationRequest);
--- notification/src/main/java/com/microservices/notification/NotificationApplication.java
+++ notification/src/main/java/com/microservices/notification/NotificationApplication.java
@@ -3,6 +3,8 @@ package com.microservices.notification;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @EnableEurekaClient
 @SpringBootApplication(
@@ -11,6 +13,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
                 "com.microservices.amqp"
         }
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class NotificationApplication {
     public static void main(String[] args) {SpringApplication.run(NotificationApplication.class, args);}
 }
